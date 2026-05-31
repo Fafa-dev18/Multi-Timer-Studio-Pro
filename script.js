b3629ae7f030bd005024a4e930890da822f02254
Fafa-dev18 <luizgustavoofc1@gmail.com>
2026-05-31 13:05:43 -0300

    algumas mudançãs na formatação dos segundo

--- script.js
+++ script.js
@@ -3,25 +3,32 @@ const pararButton = document.querySelector("#parar")
 const reiniciarButton = document.querySelector("#reiniciar")
 
 let number = 0
-
 let cron
-
 let h2 = document.querySelector("h2")
+
+function formatarTempo(totalSegundos) {
+    const minutos = Math.floor(totalSegundos / 60).toString().padStart(2, "0");
+    const segundos = (totalSegundos % 60).toString().padStart(2, "0");
+    
+    return `${minutos}:${segundos}`;
+}
 
 function start(){
     clearInterval(cron);
-    cron = setInterval( function(){
-        number++
-        h2.innerText = number;
-    },1000)
+    cron = setInterval(function(){
+        number++;
+        h2.innerText = formatarTempo(number);
+    }, 1000)
 }
+
 function stop(){
     clearInterval(cron);
 }
+
 function reset(){
     clearInterval(cron);
-    number = 0
-    h2.innerText = number
+    number = 0;
+    h2.innerText = formatarTempo(number);
 }
 
 iniciarButton.addEventListener("click", start)
